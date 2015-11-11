2ff5e1bfb7120eb88a8b3375668853e62baa4eec
Mårten Nilsson <nilssonmarten@live.se>
2015-11-11 09:25:58 +0100

    Created some stuff

--- CircleButtonApp.java
+++ CircleButtonApp.java
@@ -1,5 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Mårten on 2015-11-11.
  */
-public class CircleButtonApp {
+public class CircleButtonApp extends JFrame{
+
+    //Reference to the frame we will use
+    static CircleButtonApp appFrame;
+
+    //constructor
+    public CircleButtonApp(){
+        super("Circle window");
+        setPreferredSize(new Dimension(500,500));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        pack();
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        new CircleButtonApp();
+    }
 }
